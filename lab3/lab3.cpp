cebeab3f95c281350287c43f0308fc70123946ab
Rolan <dragon2017aaa@gmail.com>
2024-10-04 20:24:12 +0200

    123

---
+++ lab3/lab3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    unsigned int x;
+    int i;
+
+    std::cout << "Введите значение числа: ";
+    std::cin >> x;
+
+    std::cout << "Введите номер бита: ";
+    std::cin >> i;
+
+    if (x > 0 && x < 1000000000 && i >= 0 && i < 32) { // проверка значенй
+        x &= ~(1 << i); // установка i-го бита в 0
+        std::cout << "Число после установки бита: " << x << std::endl;
+    } else {
+        std::cout << "Введены неправильные данные" << std::endl;
+    }
+
+    return 0;
+}
