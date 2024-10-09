4758c23069c31bc9e816a232b7c1cdcb5ce0e617
Rolan <dragon2017aaa@gmail.com>
2024-10-09 14:30:55 +0200

    123123

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,3 +1,54 @@
-//
-// Created by Rolan Shvetsov on 09.10.2024.
-//
+#include <iostream>
+
+int main() {
+    int A, B, C;
+    std::cout << "Введите натуральные числа A, B и C: ";
+    std::cin >> A >> B >> C;
+
+    if (A < B && B < C) {
+        std::cout << "C - B - A = " << C - B - A << std::endl;
+    } else if (A % C == 0) {
+        std::cout << "A / C + B = " << A / C + B << std::endl;
+    } else {
+        std::cout << "A + B + C = " << A + B + C << std::endl;
+    }
+
+    int N;
+    std::cout << "Введите код товара (1-5): ";
+    std::cin >> N;
+
+    switch (N) {
+    case 1:
+        std::cout << "Товар №1: Яблоко" << std::endl;
+        break;
+    case 2:
+        std::cout << "Товар №2: Банан" << std::endl;
+        break;
+    case 3:
+        std::cout << "Товар №3: Морковь" << std::endl;
+        break;
+    case 4:
+        std::cout << "Товар №4: Картошка" << std::endl;
+        break;
+    case 5:
+        std::cout << "Товар №5: Хлеб" << std::endl;
+        break;
+    default:
+        std::cout << "Неверный код товара" << std::endl;
+        break;
+    }
+
+    int x;
+    std::cout << "Введитеx (-1 либо 1): ";
+    std::cin >> x;
+
+    if (x == -1) {
+        std::cout << "Negative number" << std::endl;
+    } else if (x == 1) {
+        std::cout << "Positive number" << std::endl;
+    } else {
+        std::cout << "Ошибка: Введено неверное число" << std::endl;
+    }
+
+    return 0;
+}
